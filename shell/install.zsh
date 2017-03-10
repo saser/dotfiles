ca5b105d8767a407226f6e2facdff4db15d1aebd
Christian Persson <saser@live.se>
2017-03-10 16:19:29 +0100

    Add basic plugin configuration and installation

--- shell/install.zsh
+++ shell/install.zsh
@@ -5,3 +5,5 @@ source ../utils.zsh
 PREFIX="Shell"
 
 safely_link zshenv ${HOME}/.zshenv
+safely_link zshrc ${HOME}/.zshrc
+safely_link zplug ${HOME}/.zplug
--- shell/uninstall.zsh
+++ shell/uninstall.zsh
@@ -5,3 +5,5 @@ source ../utils.zsh
 PREFIX="Shell"
 
 safely_unlink ${HOME}/.zshenv
+safely_unlink ${HOME}/.zshrc
+safely_unlink ${HOME}/.zplug
