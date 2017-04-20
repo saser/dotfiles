b63c281235a0c85c1287a0639a9182ae070c865e
Christian Persson <saser@live.se>
2017-04-20 11:25:59 +0200

    [general] Remove zsh helper functions that are no longer used

--- install_exports.zsh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/zsh
-
-export DOTS_HOME=${HOME}/dotfiles
--- uninstall.zsh
+++ uninstall.zsh
@@ -5,27 +5,11 @@ set -o pipefail
 
 STOW_TARGET="${HOME}"
 
-INSTALL_EXPORTS="${HOME}/dotfiles/install_exports.zsh"
-if [[ ! -e "${INSTALL_EXPORTS}" ]]; then
-    echo "Required file \`install_exports.zsh\` was not found at its expected location \`${HOME}/dotfiles/install_exports.zsh\`."
-    echo "Aborting."
-    exit 1
-fi
-
-source ${INSTALL_EXPORTS}
-source ${DOTS_HOME}/utils.zsh
-
-PREFIX="All"
-
 # Uninstall shell configuration.
 stow --target ${STOW_TARGET} --delete shell
-ok "Uninstalled shell configuration."
 
 # Uninstall tmux configuration.
 stow --target ${STOW_TARGET} --delete tmux
-ok "Uninstalled tmux configuration."
 
 # Uninstall neovim configuration.
-# NOTE: does _not_ uninstall the plugins.
 stow --target ${STOW_TARGET} --delete neovim
-ok "Uninstalled neovim configuration."
