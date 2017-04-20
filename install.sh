cd6b3a254834968277ab1ec8b683d2c3d7628440
Christian Persson <saser@live.se>
2017-04-20 12:16:02 +0200

    [general] Safely create the `~/.config` directory before linking

--- install.sh
+++ install.sh
@@ -3,6 +3,24 @@ set -e
 set -u
 set -o pipefail
 
+# Since some of my stuff is installed to `~/.config`, I want to make sure that
+# directory exists before stowing -- if it does not, Stow _might_ create a
+# symbolic link named `~/.config` pointing into this repository. This is bad,
+# since some applications I use put their default configuration in `~/.config`
+# and I do not want their configuration to show up in this repository.
+if [ -e "${HOME}/.config" ]; then
+    if [ ! -d "${HOME}/.config" ]; then
+        echo "`${HOME}/.config` exists but is not a directory -- this seems wrong!"
+	echo "Not installing."
+	exit 1
+    fi
+    # Otherwise, `~/.config` exists and is a directory -- we do not need to do
+    # anything.
+fi
+# If we get to this point, we know that `~/.config` does not exist, so create
+# it.
+mkdir -p ${HOME}/.config
+
 STOW_TARGET="${HOME}"
 
 # Install shell configuration.
