5e26d8a408aa8802dc49528a1509306f3b8c05f4
Christian Persson <saser@live.se>
2017-07-24 09:01:56 +0200

    [shell] Add handy aliases for interacting with clipboard

--- shell/.zsh-extras/aliases.zsh
+++ shell/.zsh-extras/aliases.zsh
@@ -4,7 +4,7 @@
 alias ..='cd ..'
 alias ls='ls -l --all --human-readable --classify --group-directories-first --color=auto'
 
-# git aliases.
+# `git` aliases.
 alias g='git'
 alias ga='g add'
 alias gs='g s' # git status
@@ -18,12 +18,26 @@ alias gcob='g cob' # git checkout -b
 alias gane='g ane' # git commit --amend --no-edit
 alias gf='g f' # git fetch
 
-# tmux aliases.
+# `tmux` aliases.
 alias t='tmux'
 alias tn='t new -s'
 alias ta='t attach'
 alias tat='ta -t' # attach to session name
 alias tl='t list-sessions'
 
-# docker aliases.
+# `docker` aliases.
 alias docker-ip="docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'"
+
+# Convenient aliases for interacting with the clipboard.  Inspired by the OS X
+# commands `pbcopy` and `pbpaste`.  These commands use stdin and stdout,
+# respectively. Convenient for e.g. copying the contents of a file, using
+# `pbcopy <file`, or e.g. pasting the current clipboard contents to a file using
+# `pbpaste >file`.
+#
+# NOTE: in X there are actually three clipboards: PRIMARY, SECONDARY, and
+# CLIPBOARD. The PRIMARY clipboard is the one that is operated using the middle
+# mouse button. The CLIPBOARD clipboard is the one that is usually operated
+# using Ctrl-C and Ctrl-V, and is the one used for these aliases. The SECONDARY
+# clipboard is rarely used.  (Taken/rephrased from xsel(1).)
+alias pbcopy='xsel --clipboard --input'
+alias pbpaste='xsel --clipboard --output'
