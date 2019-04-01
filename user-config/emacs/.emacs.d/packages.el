4fd7c9571ec6b9458f94ab4fbe98504ba8ea05cc
Christian Persson <saser@live.se>
2019-04-01 11:39:32 +0200

    emacs: use proper combination of backtick and single quote

--- user-config/emacs/.emacs.d/packages.el
+++ user-config/emacs/.emacs.d/packages.el
@@ -1,16 +1,16 @@
-; `use-package` is a package itself. It provides a nicer way to define packages,
+; `use-package' is a package itself. It provides a nicer way to define packages,
 ; handle their installation (using the `:straight` keyword), and define settings
 ; and keybindings related to those packages.
 (straight-use-package 'use-package)
 
 ;;; Editing packages
 
-; `evil` provides Vim emulation for Emacs. Many keybindings from Vim exist in
-; `evil` as well.
+; `evil' provides Vim emulation for Emacs. Many keybindings from Vim exist in
+; `evil' as well.
 (use-package evil
   :straight t
   :config
-  ; `evil-mode` needs to be enabled after `evil` is loaded.
+  ; `evil-mode' needs to be enabled after `evil' is loaded.
   (evil-mode 1))
 
 ;;; Visual packages
@@ -26,8 +26,8 @@
 
 ; Intero is a fully-fledged Haskell development environment, with many nice
 ; features such as "type of selection", a REPL, and specifically, integration
-; with `stack`.
+; with `stack'.
 (use-package intero
   :straight t
-  ; Intero should be hooked into `haskell-mode`, in order to activate.
+  ; Intero should be hooked into `haskell-mode', in order to activate.
   :hook (haskell-mode . intero-mode))
