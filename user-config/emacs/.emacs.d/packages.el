84897e4b03dc3b43f7a63b5d77097f10924f0c3c
Christian Persson <saser@live.se>
2019-05-30 18:08:08 +0200

    emacs: add some buffer-related keybinds

--- user-config/emacs/.emacs.d/packages.el
+++ user-config/emacs/.emacs.d/packages.el
@@ -69,6 +69,10 @@
   ;;; `general' keybinds.
   ;; Set up `SPC h' to be the new `help-command' binding.
   (leader-def "h" 'help-command)
+  ;;; Buffer-related keybinds.
+  (leader-def
+    "b b" 'switch-to-buffer
+    "b k" 'kill-buffer)
   ;; Also set up `<F1>' to be an escape-hatch `help-command', in case I am in
   ;; some major mode where `SPC h' does not work.
   (general-define-key "<F1>" 'help-command)
