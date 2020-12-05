8ba288165f5e1efd2b67afe30c4f05e4891e5d55
Christian Persson <Saser@users.noreply.github.com>
2020-12-05 09:34:36 +0100

    emacs: bootstrap `straight.el` and `use-package`

---
+++ ansible/roles/emacs/files/.emacs.d/init.el
@@ -0,0 +1,11 @@
+;; === PACKAGE MANAGER SETUP ===
+;; This bootstraps the `straight.el' package manager and the `use-package'
+;; configuration manager to work nicely together.
+;;
+;; First, load straight.el from disk.
+(load (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+;; Then, install the `use-package' package, using straight.el.
+(straight-use-package 'use-package)
+;; Finally, set up `use-package' to always use `straight.el' for installing
+;; packages.
+(setq straight-use-package-by-default t)
