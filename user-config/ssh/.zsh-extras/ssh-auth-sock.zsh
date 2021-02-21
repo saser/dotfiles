e2b1f107d83f8309b4a91ecac66719395fda3a6c
Christian Persson <Saser@users.noreply.github.com>
2021-02-21 01:00:14 +0100

    user-config: remove `ssh`
    All the relevant configuration has now been moved over to the
    Ansible-based dotfiles, so this is no longer needed.

--- user-config/ssh/.zsh-extras/ssh-auth-sock.zsh
+++
@@ -1,4 +0,0 @@
-# This should be equivalent to the systemd service. The `%t` in the path of the
-# systemd service is equivalent to `${XDG_RUNTIME_DIR}` when using the user
-# manager of systemd (i.e. `systemd --user`).
-export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/ssh-agent.socket"
