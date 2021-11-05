d55fedba54347562fdae09ab399a5fdae27b5b40
Christian Persson <Saser@users.noreply.github.com>
2021-11-05 21:46:05 +0000

    git: simplify role and enable it

---
+++ roles/git/files/aliases.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+alias g='git'
+alias ga='g add'
+alias gs='g s' # git status
+alias gl='g l' # one-line log
+alias gd='g d' # git diff
+alias gdc='g dc' # git diff --cached
+alias gc='g c' # git commit
+alias gcm='g cm' # git commit -m
+alias gco='g co' # git checkout
+alias gcob='g cob' # git checkout -b
+alias gane='g ane' # git commit --amend --no-edit
+alias gf='g f' # git fetch
