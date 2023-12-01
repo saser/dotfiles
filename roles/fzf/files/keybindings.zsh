4332c06a721466fc7dd4e7efabc1413235dfbcb8
Christian Persson <saser@users.noreply.github.com>
2023-12-01 21:33:59 +0000

    fzf: tidy up role and enable it for macOS.

--- roles/fzf/files/keybindings.zsh
+++ roles/fzf/files/keybindings.zsh
@@ -1,3 +1,9 @@
 #!/usr/bin/env zsh
 
-source /usr/share/fzf/key-bindings.zsh
+if [[ "$(uname -s)" = "Darwin" ]]; then
+    source "$(brew --prefix)/opt/fzf/shell/completion.zsh"
+    source "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh"
+else
+    source /usr/share/fzf/completion.zsh
+    source /usr/share/fzf/key-bindings.zsh
+fi
