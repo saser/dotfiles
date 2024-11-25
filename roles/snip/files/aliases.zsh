59d6be9d507e244367c20d9e79355636be84a2b1
Christian Persson <saser@users.noreply.github.com>
2024-11-25 10:35:23 +0000

    snip: add aliases to read and writes notes with an `#inbox` tag.

--- roles/snip/files/aliases.zsh
+++ roles/snip/files/aliases.zsh
@@ -5,3 +5,18 @@ alias sm='snip -m'
 alias sme='snip -edit -m'
 alias st='cat ~/.snip/$(date -I).txt'
 alias ste='vim ~/.snip/$(date -I).txt'
+
+# "snip inbox"
+si () {
+    snip -m "$(printf "#inbox %s" "${1}")"
+}
+
+# "snip inbox edit"
+sie () {
+    snip -edit -m "$(printf "#inbox %s" "${1}")"
+}
+
+# "snip inbox get"
+sig () {
+    cat ~/.snip/*.txt | grep -F '#inbox' | sort
+}
