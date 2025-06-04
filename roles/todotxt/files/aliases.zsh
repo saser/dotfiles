584025a6d2bab86cbc424d34dd9e1c78db6de51e
Christian Persson <582273+saser@users.noreply.github.com>
2025-06-04 16:15:50 +0200

    todotxt: configure alias to directly edit todo.txt.

--- roles/todotxt/files/aliases.zsh
+++ roles/todotxt/files/aliases.zsh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 
-alias td='todo.sh -a -t'
+alias td='todo.sh -a -t'         # 'todo'
+alias tde='vim ~/todo/todo.txt'  # 'todo edit' -- really wish todo.sh provided this natively
